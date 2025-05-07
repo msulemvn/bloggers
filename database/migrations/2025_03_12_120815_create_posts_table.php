1c663139e019adbcdc44b3c57d4deacd96b8bc6e
Muhammad Suleman <msulemvn@gmail.com>
2025-05-07 22:49:09 +0500

    fix post table slug

--- database/migrations/2025_03_12_120815_create_posts_table.php
+++ database/migrations/2025_03_12_120815_create_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->text('title');
-            $table->text('slug')->unique();
+            $table->string('slug')->unique();
             $table->foreignId('user_id')->constrained('users');
             $table->string('description');
             $table->text('content');
